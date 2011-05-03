6e9038307e2faa2ae8febec7c3f80ac21e40d57a
piac <giulio@mcneel.com>
2011-05-03 10:15:00 +0200

    sampleScript.py shows how to see if ghdoc variable is set and branch rs outputs

--- Samples/sampleScript.py
+++ Samples/sampleScript.py
@@ -1,9 +1,26 @@
 ﻿import math
 import rhinoscriptsyntax as rs
 
-result = []
-for i in range(x):
-    cId = rs.AddCircle([i, math.cos(i), 0], 0.3)
-    result.append(cId)
+if x is None:
+    x = 100
 
-a = ghdoc[result]
+circles = []
+radii = []
+for i in range(x):
+    pt = (i, math.cos(i), 0)
+    
+    cId = rs.AddCircle(pt, 0.3)
+    circles.append(cId)
+    
+    rId = rs.AddLine(pt, rs.PointAdd(pt, (0, 0.3, 0)))
+    radii.append(rId)
+
+# Check if we are in Grasshopper
+# with Grasshopper document override
+if "ghdoc" in globals():
+    #a = ghdoc
+    
+    # If we need to separate different results,
+    # this will do:
+    a = ghdoc[circles]
+    #r = ghdoc[radii]
