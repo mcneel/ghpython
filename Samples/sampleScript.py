b3c61eb0f136dbb465680e57bee8a37f688b7313
piac <giulio@mcneel.com>
2011-07-19 23:54:49 +0200

    sample script simpler

--- Samples/sampleScript.py
+++ Samples/sampleScript.py
@@ -9,17 +9,11 @@ radii = []              # ...and another one
 
 for i in range(x):
     pt = (i, math.cos(i), 0)             # a tuple (here for a point)
-    
     id1 = rs.AddCircle(pt, 0.3)
     circles.append(id1)
     endPt = rs.PointAdd(pt, (0, 0.3, 0)) # move the point by the vector
     id2 = rs.AddLine(pt, endPt)
     radii.append(id2)
 
-# check if we are in Grasshopper with the ghdoc variable
-if "ghdoc" in globals():
-    a = ghdoc
-    
-    # otherwise, to separate results
-    # a = ghdoc.SubSet(circles)
-    # b = ghdoc.SubSet(radii)
+a = circles
+b = radii
