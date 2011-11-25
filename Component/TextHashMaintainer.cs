f97cb836718fb1cbe65f8600ba3d07fbfc70eed2
piac <giulio@mcneel.com>
2011-11-25 13:55:26 +0100

    minor compatibility changes

--- Component/TextHashMaintainer.cs
+++ Component/TextHashMaintainer.cs
@@ -34,10 +34,12 @@ namespace GhPython.Component
             if (hash == null || other == null)
                 return hash == other;
 
+            bool equal = false;
+
             if (hash.Length == other.Length)
             {
-                bool equal = true;
-
+                equal = true;
+                
                 for (int i = 0; i < hash.Length; i++)
                 {
                     if (hash[i] != other[i])
@@ -46,14 +48,13 @@ namespace GhPython.Component
                         break;
                     }
                 }
-                return equal;
             }
-            return false;
+            return equal;
         }
 
         private byte[] ComputeHash(string text)
         {
-            using(var p = new SHA512CryptoServiceProvider())
+          using (var p = SHA256.Create())
             {
                 return p.ComputeHash(Encoding.UTF8.GetBytes(text));
             }
