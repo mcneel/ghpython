3c9eb387c11c1ca4cf792b18d9548250ec2f8852
sbaer <steve@mcneel.com>
2013-11-04 16:20:01 -0800

    added optional flatten_results parameter to flatten "lists of lists"

--- package/parallel.py
+++ package/parallel.py
@@ -1,12 +1,15 @@
 import System.Threading.Tasks as tasks
 from System import Exception, AggregateException
 
-def run(function, data_list):
+def run(function, data_list, flatten_results = False):
     """for each item in data_list execute the input function. Execution is
     done on as many threads as there are CPUs on the computer.
     Parameters:
         function: function to execute for each item in the data_list
         data_list: list, tuple, or other enumerable data structure
+        flatten_list [opt]: if True, when results are lists of lists the
+          results are flattened into a single list of results. If this is True,
+          you cannot depend on the result list being the same size as the input list
     Returns:
         list of results containing the return from each call to the input function
     """
@@ -22,4 +25,12 @@ def run(function, data_list):
     helper(pieces[0])
     pieces = pieces[1:]
     if pieces: tasks.Parallel.ForEach(pieces, helper)
+    if flatten_results:
+        temp = []
+        for result in results:
+            if type(result) is list:
+                for r in result: temp.append(r)
+            else:
+                temp.append(result)
+        results = temp
     return results
